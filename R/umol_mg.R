95ee8b77d6e175914d5d94008a74256b7f3ea639
Erin Reilly <erinmariereilly@gmail.com>
2019-12-05 15:10:34 -0500

    Okay this time i really fixed it.  I think.

--- R/umol_mg.R
+++ R/umol_mg.R
@@ -1,3 +1,14 @@
+#' Use the NASL formatted data and convert from mg/L to umol/L
+#'
+#' This function creates a new column with values in umols/L from the mg/L results
+#' This only works for Nitrogen, Phosphorous, Carbon and Silica in mg/L
+#' All other units will be listed as NA
+#'
+#' @param data dataframe formated using the load_nasl function
+#' @return A dataframe with an extra column
+#' @export
+
+
 mg_umol<-function(x){
   library(dplyr)
   x<-x %>%
